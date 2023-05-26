66bdfed374d1652a1a8491060dd509501b1f394f
ImperialRed5 <rosay_21@icloud.com>
2023-05-25 20:48:33 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,21 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
 const Form = () => {
   return (
-    <div id='container'>
-      <div id='navbar'>
-        Form.js
-      </div>
-      <form>
-        {/* your form fields here */}
-      </form>
+    <div id="container">
+      <div id="navbar">Form.js</div>
+      <form>{/* your form fields here */}</form>
     </div>
-  )
-}
+  );
+};
 
-
-ReactDOM.render(
-  <Form />,
-  document.getElementById('app')
-)
+ReactDOM.render(<Form />, document.getElementById("app"));
